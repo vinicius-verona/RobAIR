857fbdd9b6f7babc1ad61a226145eeccb5b8a835
Vinicius Verona <vinicius.verona@aluno.ufop.edu.br>
2023-07-03 13:11:34 +0200

    Update deploy script

--- deploy_code_to_robot.sh
+++ deploy_code_to_robot.sh
@@ -3,9 +3,10 @@
 if [[ -n "$ROS_MASTER_URI" ]]; then
     echo "Deploying code to robair with IP ($ROS_MASTER_URI)"
     ros_ip_address=$(echo "$ROS_MASTER_URI" | cut -d'/' -f3 | cut -d':' -f1)
+    
     if ping -q -W 5 -c 5 $ros_ip_address >/dev/null; then
-        scp -r $HOME/RobAIR-DEV/RobAIR robair@$ros_ip_address:~/vinicius-patrol-robot/RobAIR
-        ssh -t robair@$ros_ip_address "sh update_catkin_package.sh && cd $HOME/RobAIR/catkin_ws && catkin_make"
+        rsync -av --exclude='.gitignore' --exclude='.git' $HOME/RobAIR-DEV/RobAIR robair@$ros_ip_address:~/vinicius-patrol-robot/RobAIR
+        ssh -t robair@$ros_ip_address "sh ~/vinicius-patrol-robot/update_catkin_package.sh"
     else
         echo "Not connected to RobAIR network!"
     fi
